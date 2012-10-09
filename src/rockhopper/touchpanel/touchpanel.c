06a658a2d9da5add16d1c222cb77b17e458e6fb9
Simon Busch <morphis@gravedo.de>
2012-10-09 06:42:25 +0200

    touchpanel: read screen size from framebuffer when possible
    * read screen size when not defined by machine config from framebuffer
    * define static screen size for qemuarm and qemux86 machines
    
    Open-webOS-DCO-1.0-Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/rockhopper/touchpanel/touchpanel.c
+++ src/rockhopper/touchpanel/touchpanel.c
@@ -44,6 +44,8 @@
 #include <dirent.h>
 #include <fcntl.h>
 
+#include <linux/fb.h>
+
 #include "touchpanel_gestures.h"
 
 /* Later versions of nyx_utils.h no longer define this macro */
@@ -301,17 +303,16 @@ static general_settings_t sGeneralSettings =
 
 static float scaleX, scaleY;
 
-/* Using hardcoded values for now ,since ioctl FBIOGET_VSCREENINFO 
-   on /dev/fb0 is returning invalid values in qemux86 */
-
-#define SCREEN_HORIZONTAL_RES	1024
-#define SCREEN_VERTICAL_RES	768
-
 static int
 init_touchpanel(void)
 {
 	struct input_absinfo abs;
 	int  maxX, maxY, ret = -1;
+#if !defined(SCREEN_HORIZONTAL_RES) && !defined(SCREEN_VERTICAL_RES)
+	struct fb_var_screeninfo scr_info;
+	int16_t fd;
+#endif
+
     	
 	touchpanel_event_fd = open("/dev/input/touchscreen0", O_RDWR);
 	if(touchpanel_event_fd < 0) {
@@ -337,8 +338,27 @@ init_touchpanel(void)
 	init_vbox_touchpanel();
     	init_gesture_state_machine(&sGeneralSettings, 1);
 
+#if !defined(SCREEN_HORIZONTAL_RES) && !defined(SCREEN_VERTICAL_RES)
+	fd = open("/dev/fb0", O_RDWR);
+	if (fd < 0) {
+		return NYX_ERROR_INVALID_FILE_ACCESS;
+	}
+
+	memset(&scr_info,0,sizeof(struct fb_var_screeninfo));
+
+	if (ioctl(fd, FBIOGET_VSCREENINFO, &scr_info) != 0) {
+		ret = NYX_ERROR_INVALID_OPERATION;
+		goto error;
+	}
+
+	close(fd);
+
+	scaleX = (float)scr_info.xres / (float)maxX;
+	scaleY = (float)scr_info.yres / (float)maxY;
+#else
 	scaleX = (float)SCREEN_HORIZONTAL_RES / (float)maxX;
 	scaleY = (float)SCREEN_VERTICAL_RES / (float)maxY;
+#endif
 
 	return 0;
 error:
