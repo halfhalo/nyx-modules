3b0af9d452c7341604393c80399587fb591f6f87
Patrick Roberts <patrick@silentbluesystems.com>
2013-02-15 16:58:13 -0800

    We don't want wacom, we want the touch panel

--- src/rockhopper/touchpanel/touchpanel.c
+++ src/rockhopper/touchpanel/touchpanel.c
@@ -336,7 +336,7 @@ init_touchpanel(void)
 #endif
 
     	
-	touchpanel_event_fd = open("/dev/input/touchscreen0", O_RDWR | O_NONBLOCK);
+	touchpanel_event_fd = open("/dev/input/event5", O_RDWR | O_NONBLOCK);
 	if(touchpanel_event_fd < 0) {
 		nyx_error("Error in opening touchpanel event device");
 		return -1;
